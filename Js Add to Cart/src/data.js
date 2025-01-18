ab4e1ca59e65216bfcbbf1bc3ef3611c785a54f8
Parth99001 <Parthgandhi350@gmail.com>
2025-01-18 15:55:56 +0530

    first commit

---
+++ Js Add to Cart/src/data.js
@@ -0,0 +1,79 @@
+let shopItemData = [{
+    id:"parth",
+    name:"Casual shirt",
+    price:45,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img:"images/img-1.jpg"
+},{
+    id:"Almin",
+    name:"Office shirt",
+    price:100,
+    desc:"Lorem ipsum dolor s   it, amet consectetur adipisicing",
+    img:"images/img-2.jpg"
+},{
+    id:"Mitan",
+    name:"T-shirt",
+    price:25,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img:"images/img-3.jpg"
+},{
+    id:"Jeet",
+    name:"Man's Suit",
+    price:145,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img:"images/img-4.jpg"
+},{
+    id:"Raj",
+    name:"Denim Jacket",
+    price:80,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img:"images/img-3.jpg"
+},{
+    id:"Sia",
+    name:"Floral Dress",
+    price:60,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img:"images/img-2.jpg"
+},{
+    id:"Lina",
+    name:"Leather Jacket",
+    price:120,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img:"images/img-1.jpg"
+},{
+    id:"Sam",
+    name:"Hoodie",
+    price:50,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img:"images/img-4.jpg"
+},{
+    id:"Amy",
+    name:"Summer Shorts",
+    price:30,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img:"images/img-2.jpg"
+},{
+    id:"Eva",
+    name:"Button-up Dress",
+    price:75,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img:"images/img-1.jpg"
+},{
+    id:"John",
+    name:"Track Pants",
+    price:35,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img:"images/img-4.jpg"
+},{
+    id:"Leo",
+    name:"Bomber Jacket",
+    price:95,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img:"images/img-3.jpg"
+},{
+    id:"Mia",
+    name:"shirt",
+    price:40,
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing",
+    img:"images/img-2.jpg"
+}]
